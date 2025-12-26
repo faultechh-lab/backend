bc443dd3f95f50490c128b048d9640620678434d
okti89 <faultechh@gmail.com>
2025-12-26 15:17:59 +0300

    update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,5 +14,14 @@ urlpatterns = [
     path('api/news/', include('news.urls')),
     path('api/notifications/', include('notifications.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += [
+        re_path(
+            r'^media/(?P<path>.*)$',
+            serve,
+            {'document_root': settings.MEDIA_ROOT}
+        ),
+    ]
