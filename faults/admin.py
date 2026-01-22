5f80eb57cfda1be5d131f13741a6a9804d0703ec
okti89 <faultechh@gmail.com>
2026-01-23 02:09:42 +0300

    update admin.py

--- faults/admin.py
+++ faults/admin.py
@@ -70,3 +70,128 @@ class ModelAdmin_(ImportExportModelAdmin):
 class SparePartImageInline(admin.TabularInline):
     model = SparePartImage
     extra = 1
+
+class FaultCodesAdmin(ImportExportModelAdmin):
+    resource_class = FaultCodesResource
+    list_display = ("code", "category", "brand", "model", "active")
+    list_filter = ("category", "brand", "model", "active")
+    search_fields = ("code", "fault_description", "brand__name", "model__name", "category__name")
+    autocomplete_fields = ("category", "brand", "model")
+    inlines = [SparePartImageInline]
+
+class ParameterImageInline(admin.TabularInline):
+    model = ParameterImage
+    extra = 1
+
+class ParameterAdmin(ImportExportModelAdmin):
+    list_display = ("name", "category", "brand", "model", "active")
+    list_filter = ("category", "brand", "model", "active")
+    search_fields = ("name", "description")
+    autocomplete_fields = ("category", "brand", "model")
+    inlines = [ParameterImageInline]
+
+class BoilerCardRepairImageInline(admin.TabularInline):
+    model = BoilerCardRepairImage
+    extra = 1
+
+class BoilerCardRepairAdmin(ImportExportModelAdmin):
+    list_display = ("title", "category", "brand", "model", "active")
+    list_filter = ("category", "brand", "model", "active")
+    search_fields = ("title", "description")
+    autocomplete_fields = ("category", "brand", "model")
+    inlines = [BoilerCardRepairImageInline]
+
+class BoilerRepairGuideAdmin(ImportExportModelAdmin):
+    list_display = ("__str__", "active",'title')
+    list_filter = ("active",)
+    search_fields = ("title",)
+
+class BoilerPartImageInline(admin.TabularInline):
+    model = BoilerPartImage
+    extra = 1
+
+class BoilerPartAdmin(ImportExportModelAdmin):
+    list_display = ("name", "category", "brand", "model", "active")
+    list_filter = ("category", "brand", "model", "active")
+    search_fields = ("name",)
+    autocomplete_fields = ("category", "brand", "model")
+    inlines = [BoilerPartImageInline]
+
+
+class SparePartsDefinitionsImageInline(admin.TabularInline):
+    model = SparePartsDefinitionsImage
+    extra = 1
+
+class SparePartsDefinitionsAdmin(ImportExportModelAdmin):
+    list_display = ("name", "active")
+    list_filter = ("active",)
+    search_fields = ("name",)
+    inlines = [SparePartsDefinitionsImageInline]
+
+class BoilerWorkingPrincipleAdmin(ImportExportModelAdmin):
+    list_display = ("title", "active")
+    list_filter = ("active",)
+    search_fields = ("title", "description")
+
+class BoilerBoardRepairerAdmin(ImportExportModelAdmin):
+    list_display = ("name", "business_type", "city", "phone_number", "active")
+    list_filter = ("business_type", "city", "active")
+    search_fields = ("name", "address", "phone_number", "email", "website")
+
+class InstrumentUsageAdmin(ImportExportModelAdmin):
+    list_display = ("__str__", "active")
+    list_filter = ("active",)
+    search_fields = ("content",)
+
+class VideoAdmin(ImportExportModelAdmin):
+    list_display = ("title", "category", "brand", "model", "active")
+    list_filter = ("category", "brand", "model", "active")
+    search_fields = ("title", "description")
+    autocomplete_fields = ("category", "brand", "model")
+
+class RoomTermostatImageInline(admin.TabularInline):
+    model = RoomTermostatImage
+    extra = 1
+
+    
+class RoomTermostatAdmin(ImportExportModelAdmin):
+    list_display = ("title", "active")
+    list_filter = ("active",)
+    search_fields = ("title", "description")
+    inlines = [RoomTermostatImageInline]
+
+class FavoriteBrandAdmin(ImportExportModelAdmin):
+    list_display = ("user", "brand")
+    list_filter = ("user", "brand")
+    search_fields = ("user__username", "brand__name")
+    
+
+class FavoriteModelAdmin(ImportExportModelAdmin):
+    list_display = ("user", "model")
+    list_filter = ("user", "model")
+    search_fields = ("user__username", "model__name")
+
+class FavoriteFaultCodeAdmin(ImportExportModelAdmin):
+    list_display = ("user", "fault")
+    list_filter = ("user", "fault")
+    search_fields = ("user__username", "fault__code")
+    
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Model, ModelAdmin_)
+admin.site.register(FaultCodes, FaultCodesAdmin)
+admin.site.register(Parameter, ParameterAdmin)
+admin.site.register(BoilerRepairGuide, BoilerRepairGuideAdmin)
+admin.site.register(BoilerPart, BoilerPartAdmin)
+admin.site.register(SparePartsDefinitions, SparePartsDefinitionsAdmin)
+admin.site.register(BoilerWorkingPrinciple, BoilerWorkingPrincipleAdmin)
+admin.site.register(BoilerCardRepair, BoilerCardRepairAdmin)
+admin.site.register(BoilerBoardRepairer, BoilerBoardRepairerAdmin)
+admin.site.register(InstrumentUsage, InstrumentUsageAdmin)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(RoomTermostat, RoomTermostatAdmin)
+admin.site.register(FavoriteBrand, FavoriteBrandAdmin)
+admin.site.register(FavoriteModel, FavoriteModelAdmin)
+admin.site.register(FavoriteFaultCode, FavoriteFaultCodeAdmin)
